5cd8233cb0917fccf3851c0c8726c083718c18ab
gokturk.dogan <gokturk.dogan@beymen.com>
2025-09-29 16:29:47 +0300

    email plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,25 @@
-module.exports = () => ({});
+module.exports = () => ({
+  email: {
+    config: {
+      provider: "strapi-provider-email-smtp",
+      providerOptions: {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        username: "highwayburger.noreply@gmail.com",
+        password: "slwlqtyotbljokho"
+      },
+      settings: {
+        defaultFrom: "highwayburger.noreply@gmail.com",
+        defaultReplyTo: "highwayburger.noreply@gmail.com"
+      }
+    }
+  },
+  "users-permissions": {
+    config: {
+      email: {
+        emailConfirmation: true
+      }
+    }
+  }
+});
