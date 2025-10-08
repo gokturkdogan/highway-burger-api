080daed4e4fb1d7f5db5616e9933fb1838d8b50d
gokturk.dogan <gokturk.dogan@beymen.com>
2025-10-08 19:14:07 +0300

    last changes

--- config/database.js
+++ config/database.js
@@ -1,22 +1,15 @@
-// path: ./config/database.js
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      connection: {
-        connectionString: env('DATABASE_URL'), // Render'dan aldığın Internal Database URL
-        ssl: {
-          rejectUnauthorized: false, // Render PostgreSQL için gerekli
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false, // Render için kritik
       },
-      pool: { 
-        min: env.int('DATABASE_POOL_MIN', 2), 
-        max: env.int('DATABASE_POOL_MAX', 10) 
-      },
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
-  };
-};
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+});
