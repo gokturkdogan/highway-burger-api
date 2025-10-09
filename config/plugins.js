1a45cf1de343418b4ff57e4f215cbdb9017a9334
gokturk.dogan <gokturk.dogan@beymen.com>
2025-10-09 09:27:41 +0300

    cdn config

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   email: {
     config: {
       provider: "strapi-provider-email-smtp",
@@ -7,19 +7,33 @@ module.exports = () => ({
         port: 465,
         secure: true,
         username: "highwayburger.noreply@gmail.com",
-        password: "slwlqtyotbljokho"
+        password: "slwlqtyotbljokho",
       },
       settings: {
         defaultFrom: "highwayburger.noreply@gmail.com",
-        defaultReplyTo: "highwayburger.noreply@gmail.com"
-      }
-    }
+        defaultReplyTo: "highwayburger.noreply@gmail.com",
+      },
+    },
   },
   "users-permissions": {
     config: {
       email: {
-        emailConfirmation: true
-      }
-    }
-  }
+        emailConfirmation: true,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 });
